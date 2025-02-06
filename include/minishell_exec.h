cf3af1d41030a0799d49cd00d497d1ea4c1c7335
Liam Cohen <licohen@paul-f3Br9s2.clusters.42paris.fr>
2025-02-06 18:48:24 +0100

    maj main

--- include/minishell_exec.h
+++ include/minishell_exec.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_exec.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: liamcohen <liamcohen@student.42.fr>        +#+  +:+       +#+        */
+/*   By: licohen <licohen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 17:32:04 by licohen           #+#    #+#             */
-/*   Updated: 2025/01/28 20:01:31 by liamcohen        ###   ########.fr       */
+/*   Updated: 2025/02/06 18:45:45 by licohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,63 +30,63 @@
 #define PATH_MAX 4096 //Max length of a path
 
 //Built-in functions
-int ft_echo(char **array, int fd_out);
-int ft_exit(t_command *cmd, int fd_out);
-int ft_pwd(int fd_out);
-int	ft_env(t_environment_var *env, int fd_out);
-int ft_cd(char **args, t_environment_var *env);
-int ft_unset(t_environment_var **env, char **args);
-int	ft_export(char *arg_1, t_environment_var *env, int fd_out);
-void	execute_builtin(t_command *cmd, t_environment_var **env);
-int	is_builtin(char *cmd);
+int                 ft_echo(char **array, int fd_out);
+int                 ft_exit(t_command *cmd, int fd_out);
+int                 ft_pwd(int fd_out);
+int	                ft_env(t_environment_var *env, int fd_out);
+int                 ft_cd(char **args, t_environment_var *env);
+int                 ft_unset(t_environment_var **env, char **args);
+int	                ft_export(char *arg_1, t_environment_var *env, int fd_out);
+void                execute_builtin(t_command *cmd, t_environment_var **env);
+int	                is_builtin(char *cmd);
 
 //utils
-int nbr_of_args(char **array);
-int is_valid_env_name(const char *name);
-int is_readonly_var(const char *name);
-t_environment_var *create_new_env_node(const char *name, const char *value);
-int	update_or_create_oldpwd(t_environment_var *env);
-int	update_or_create_pwd(t_environment_var *env);
-int	cd_to_oldpwd(t_environment_var *env);
-int	cd_with_tild(char **args, t_environment_var *env);
-void    free_array(char **array);
+int                 nbr_of_args(char **array);
+int                 is_valid_env_name(const char *name);
+int                 is_readonly_var(const char *name);
+t_environment_var   *create_new_env_node(const char *name, const char *value);
+int	                update_or_create_oldpwd(t_environment_var *env);
+int	                update_or_create_pwd(t_environment_var *env);
+int	                cd_to_oldpwd(t_environment_var *env);
+int	                cd_with_tild(char **args, t_environment_var *env);
+void                free_array(char **array);
 
 //memory management 
-void cleanup_ptr(void *ptr);
-void cleanup_env_node(t_environment_var *node);
-void cleanup_environment(t_environment_var *environment);
-void cleanup_command(t_command *cmd);
-void cleanup_all(t_environment_var *env, t_command *cmd, int exit_code);
+void                cleanup_ptr(void *ptr);
+void                cleanup_env_node(t_environment_var *node);
+void                cleanup_environment(t_environment_var *environment);
+void                cleanup_command(t_command *cmd);
+void                cleanup_all(t_environment_var *env, t_command *cmd, int exit_code);
 
 
 //init
-t_environment_var *initialize_shell(char **envp);
-t_environment_var *init_environment(char **envp);
-t_environment_var *create_environment_node(char *envp);
+t_environment_var   *initialize_shell(char **envp);
+t_environment_var   *init_environment(char **envp);
+t_environment_var   *create_environment_node(char *envp);
 
 //exec
-char *get_env_value(t_environment_var *environment, const char *key);
-char *find_command_path(const char *command, t_environment_var *environment);
-int execute_command(t_command *cmd, t_environment_var *environment);
-t_cmd_type get_command_type(t_command *cmd, t_environment_var *environment);
-enum e_state check_command_type(const char *cmd);
+char                *get_env_value(t_environment_var *environment, const char *key);
+char                *find_command_path(const char *command, t_environment_var *environment);
+int                 execute_command(t_command *cmd, t_environment_var *environment);
+t_cmd_type          get_command_type(t_command *cmd, t_environment_var *environment);
+enum e_state        check_command_type(const char *cmd);
 
 //env
-int create_env_var(t_environment_var **environment, const char *name, const char *value);
-int custom_setenv(t_environment_var **environment, const char *name, const char *value);
-char *get_env_value(t_environment_var *environment, const char *key);
-char    **convert_env_to_array(t_environment_var *environment);
+int                 create_env_var(t_environment_var **environment, const char *name, const char *value);
+int                 custom_setenv(t_environment_var **environment, const char *name, const char *value);
+char                *get_env_value(t_environment_var *environment, const char *key);
+char                **convert_env_to_array(t_environment_var *environment);
 
 //redir
-int setup_redirections(t_command *cmd);
-int setup_output_fd(t_command *cmd, int flags, int *prev_fds);
-int check_output_permissions(t_command *cmd, int *prev_fds);
-int check_input_permissions(t_command *cmd, int *prev_fds);
-int     setup_fd_backup(int *prev_in_fd, int *prev_out_fd);
-void    cleanup_fds(int prev_in_fd, int prev_out_fd);
+int                 setup_redirections(t_command *cmd);
+int                 setup_output_fd(t_command *cmd, int flags, int *prev_fds);
+int                 check_output_permissions(t_command *cmd, int *prev_fds);
+int                 check_input_permissions(t_command *cmd, int *prev_fds);
+int                 setup_fd_backup(int *prev_in_fd, int *prev_out_fd);
+void                cleanup_fds(int prev_in_fd, int prev_out_fd);
 
 //process 
-int     wait_for_child(pid_t pid);
+int                 wait_for_child(pid_t pid);
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -27,7 +27,6 @@ static int execute_pipeline(t_command *cmd_list, t_environment_var *env)
     exit_code = 0;
     
     // Pour l'instant, exécute la première commande
-    // Cette partie sera étendue pour gérer les pipes
     if (current)
         exit_code = execute_command(current, env);
 
@@ -46,38 +45,27 @@ int main(int __attribute__((unused)) argc,
     t_token *token_list;
     t_command *cmd_list;
     t_environment_var *env;
-
-    // Initialisation de l'environnement
     env = initialize_shell(envp);
     if (!env)
         return (1);
 
     while (1)
     {
-        // Lecture de l'entrée avec gestion du Ctrl+D
         line = readline("minishell> ");
         if (!line)
         {
             ft_printf("exit\n");
             break;
         }
-
-        // Gestion des lignes vides et ajout à l'historique
         if (is_null_line(line) || is_void_case(line))
             continue;
         add_history(line);
-
-        // Validation et filtrage de la ligne
         line = filtered_line(line);
         if (!line)
             continue;
-
-        // Tokenisation de la ligne
         token_list = tokenize_line(line);
         if (!token_list)
             continue;
-
-        // Parsing des tokens en commandes
         cmd_list = parse_token_list(token_list, env);
         if (!cmd_list)
             continue;
@@ -85,21 +73,14 @@ int main(int __attribute__((unused)) argc,
         printf("Command: %s\n", cmd_list->args[0]);  // Devrait être "echo"
         printf("First arg: %s\n", cmd_list->args[1]); // Devrait être votre texte
         printf("Output FD: %d\n", cmd_list->output_fd); // Important pour la sortie!
-        // Expansion des variables et suppression des quotes
         if (!expand_args(cmd_list, env))
         {
             free_cmd_list(cmd_list);
             continue;
         }
-
-        // Exécution du pipeline de commandes
         env->last_exit_code = execute_pipeline(cmd_list, env);
-
-        // Nettoyage après l'exécution
         free_cmd_list(cmd_list);
     }
-
-    // Nettoyage final
     cleanup_all(env, NULL, 0);
     clear_history();
     return (0);
