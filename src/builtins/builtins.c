08ddb38961c4dd022401d211f61b91d7be4b4ee8
julienLombard <julienlombard.fr@gmail.com>
2025-02-18 22:36:30 +0100

    fix leaks in is_builtin function

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -15,6 +15,8 @@
 
 int	is_builtin(char *cmd)
 {
+	if (!cmd)
+		return (FALSE);
 	return (ft_strcmp(cmd, "echo") == 0 || ft_strcmp(cmd, "cd") == 0
 		|| ft_strcmp(cmd, "pwd") == 0 || ft_strcmp(cmd, "export") == 0
 		|| ft_strcmp(cmd, "unset") == 0 || ft_strcmp(cmd, "env") == 0
